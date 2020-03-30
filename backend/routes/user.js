19199c092017fea12fae43d808f2091eea5daca4
matisse varlin <matisse.varlin@hotmail.fr>
2020-03-30 20:20:39 +0200

    Ajout des Models + Controllesr

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getAll } from '../controllers/userController';
+
+let userRouter= express.Router();
+
+userRouter.get('/', getAll);
+
+export default userRouter;
